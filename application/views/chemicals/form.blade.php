109b2857616679c826a1345a1ccd3d553c3de90d
Freelancer <freelancer558@gmail.com>
2013-01-15 22:45:21 +0700

    remove placeholder

--- application/views/chemicals/form.blade.php
+++ application/views/chemicals/form.blade.php
@@ -1,10 +1,10 @@
 {{ $form->open() }}
 
       {{ $form->text('name', 'Name', ' ') }}
-      {{ $form->text('sum', 'Total', '', array('placeholder'=>'Unit is Kilograms')) }}
+      {{ $form->text('sum', 'Total')) }}
+      {{ $form->select('chemical_types', 'Chemical Type', $chemical_types, false, array('style'=>'width:220px;')) }}
       {{ $form->text('mfd', 'Date of manufacture') }}
       {{ $form->text('exp', 'Expire Date') }}
-      {{ $form->select('chemical_types', 'Chemical Type', $chemical_types, false, array('style'=>'width:220px;')) }}
       <!-- {{ $form->file('photo', "Photo") }} -->
       {{ $form->submit_primary('Save') }}
 {{ $form->close() }}
